04159c6e59ab2a70e39b8c937f9e3f0bf4c5933d
vpiszz <sosofiapopovaaktobe2006@gmail.com>
2025-01-29 20:50:08 +0500

    Create Program6789.cs

---
+++ Program6789.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Vehicle
+{
+    public virtual void Drive()
+    {
+        Console.WriteLine("Транспортное средство едет.");
+    }
+}
+
+public class Car : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Машина едет по дороге.");
+    }
+}
+
+public class Bicycle : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Велосипед едет по тротуару.");
+    }
+}
+
+public class Boat : Vehicle
+{
+    public override void Drive()
+    {
+        Console.WriteLine("Лодка плывёт по воде.");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Vehicle[] vehicles = new Vehicle[]
+        {
+            new Car(),
+            new Bicycle(),
+            new Boat()
+        };
+
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.Drive();
+        }
+    }
+}
